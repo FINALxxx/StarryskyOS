43225ad75294b986c60a3f945b49ade81b9b574a
FINALxxx <3085378853@qq.com>
2026-06-17 16:36:04 +0800

    init

--- User/main.c
+++ User/main.c
@@ -1,8 +1,27 @@
 #include "main.h"
 
-void main(void){
-    
-    hal_sys_uart_init();
-    hal_sys_putstr("Hello, World!\n\r");
+void easy_print(void* param){
+    printf("I will print: %s\r\n", (char*)param);
+}
 
+// Tips: Letter-shell uses slow sys_uart leaving hp_uart available for high-demand situation.
+//       if there exists a communication loss, it'll be fine by setting uart boudrate to 9600.
+void main(){
+    hal_sys_uart_init();
+
+    func_node func_node = {
+        .func = easy_print,
+        .param = "Hello, World",
+        .next = NULL,
+    };
+    set_fnode(&func_node);
+
+    create_shell_env_varible();
+
+    load_shell();
+
+    while(1){
+        printf("Never Reaching here.\r\n");
+    }
+    
 }
--- User/main.h
+++ User/main.h
@@ -2,6 +2,8 @@
 #include "board.h"
 #include "libgcc.h"
 #include "hal_sys_uart.h"
+#include "shell_init.h"
+#include "shell_port.h"
 
 // System Configuration
 #include "generated/autoconf.h"
